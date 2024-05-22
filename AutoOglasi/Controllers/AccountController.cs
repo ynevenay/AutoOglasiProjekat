eede8c605b20b231885dd8ffd81bbbfd884efb4d
Nevena Milenkovic <nmilenkovic1121it@raf.rs>
2024-05-22 22:58:55 +0200

    Inicijalizovan account kontroler

---
+++ AutoOglasi/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using AutoOglasi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace AutoOglasi.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+        }   
+    }
+}
