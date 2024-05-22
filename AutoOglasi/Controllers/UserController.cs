dd112207e9eaeb919425df56269a7c38e1acd4a2
Nevena Milenkovic <nmilenkovic1121it@raf.rs>
2024-05-22 17:30:59 +0200

    Napravljene role za korisnike

--- AutoOglasi/Controllers/UserController.cs
+++ AutoOglasi/Controllers/UserController.cs
@@ -82,5 +82,23 @@ namespace AutoOglasi.Controllers
             }
             return View(user);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(UserRole userRole)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await _roleManager.CreateAsync(new ApplicationRole() { Name = userRole.RoleName });
+                if (result.Succeeded)
+                    ViewBag.Message = "Rola uspesno kreirana";
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                        ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View();
+        }
+
     }
 }
