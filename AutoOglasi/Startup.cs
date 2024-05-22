625ef226c8ac516aced966063948b77b7fdfb9e7
Nevena Milenkovic <nmilenkovic1121it@raf.rs>
2024-05-22 22:56:48 +0200

    Konfiguracija startup.cs-a sa autentifikacijom

--- AutoOglasi/Startup.cs
+++ AutoOglasi/Startup.cs
@@ -46,6 +46,31 @@ namespace AutoOglasi
             services.AddScoped(sp => sp.GetRequiredService<IMongoClient>().GetDatabase("AutoOglasiDB"));
         }
 
-        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
     }
 }
