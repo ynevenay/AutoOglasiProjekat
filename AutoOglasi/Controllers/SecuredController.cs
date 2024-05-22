8f71bd0a2cada5016a3403708ba369d863c8e005
Nevena Milenkovic <nmilenkovic1121it@raf.rs>
2024-05-22 21:10:52 +0200

    Kreirala kontroler za administratore

---
+++ AutoOglasi/Controllers/SecuredController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoOglasi.Controllers
+{
+    public class SecuredController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
