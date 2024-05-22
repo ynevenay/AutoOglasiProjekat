03724df470294539dc90a9325243b64d16cab261
Nevena Milenkovic <nmilenkovic1121it@raf.rs>
2024-05-22 16:05:36 +0200

    Napravila user kontroler

---
+++ AutoOglasi/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using AutoOglasi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+
+namespace AutoOglasi.Controllers
+{
+    public class UserController : Controller
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private RoleManager<ApplicationRole> _roleManager;
+
+        public UserController(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
+        {
+            this._userManager = userManager;
+            this._roleManager = roleManager;
+        } 
+    }
+}
