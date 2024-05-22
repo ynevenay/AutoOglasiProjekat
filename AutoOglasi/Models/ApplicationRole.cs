fee8328e1ab1c57f436a2bafd54b22ff6823f830
Luka Lazarevic <lukalazarevic001@gmail.com>
2024-05-22 14:43:50 +0200

    Dodao AplicationUser i AplicationRole

---
+++ AutoOglasi/Models/ApplicationRole.cs
@@ -0,0 +1,11 @@
+﻿using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDbGenericRepository.Attributes;
+using System;
+
+namespace AutoOglasi.Models
+{
+    [CollectionName("Roles")]
+    public class ApplicationRole:MongoIdentityRole<Guid>
+    {
+    }
+}
