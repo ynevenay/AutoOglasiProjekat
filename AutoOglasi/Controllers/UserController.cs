87325f005af0b0142c322a7230740b553978f19b
Nevena Milenkovic <nmilenkovic1121it@raf.rs>
2024-05-22 17:00:33 +0200

    Korisnik sad moze da se kreira

--- AutoOglasi/Controllers/UserController.cs
+++ AutoOglasi/Controllers/UserController.cs
@@ -1,4 +1,4 @@
-﻿using AutoOglasi.Models;
+using AutoOglasi.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
@@ -16,6 +16,71 @@ namespace AutoOglasi.Controllers
         {
             this._userManager = userManager;
             this._roleManager = roleManager;
-        } 
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser appUser = new ApplicationUser
+                {
+                    UserName = user.Name,
+                    Email = user.Email
+                };
+
+                IdentityResult result = await _userManager.CreateAsync(appUser, user.Password);
+
+                if (result.Succeeded)
+                {
+                    // Proveri sigurnosni pečat
+                    var securityStamp = await _userManager.GetSecurityStampAsync(appUser);
+                    if (string.IsNullOrEmpty(securityStamp))
+                    {
+                        appUser.SecurityStamp = Guid.NewGuid().ToString();
+                        var updateResult = await _userManager.UpdateAsync(appUser);
+                        if (!updateResult.Succeeded)
+                        {
+                            foreach (IdentityError error in updateResult.Errors)
+                            {
+                                ModelState.AddModelError("", error.Description);
+                            }
+                            return View(user);
+                        }
+                    }
+
+                    // Dodaj korisnika u rolu
+                    var roleResult = await _userManager.AddToRoleAsync(appUser, "Admin");
+                    if (!roleResult.Succeeded)
+                    {
+                        foreach (IdentityError error in roleResult.Errors)
+                        {
+                            ModelState.AddModelError("", error.Description);
+                        }
+                        return View(user);
+                    }
+
+                    ViewBag.Message = "Nalog je uspešno kreiran";
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(user);
+        }
     }
 }
