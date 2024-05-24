9f7435500839341eebac87160d13eaef8abd5c33
Luka Lazarevic <lukalazarevic001@gmail.com>
2024-05-24 14:24:28 +0200

    Model Car napravljen

---
+++ AutoOglasi/Models/Car.cs
@@ -0,0 +1,49 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace AutoOglasi.Models
+{
+    public class Car
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonRequired]
+        public string UserId { get; set; }
+
+        [BsonElement("Brand")]
+        [Required(ErrorMessage = "Proizvodjac je obavezan")]
+        public string Brand { get; set; }
+
+        [BsonElement("Model")]
+        [Required(ErrorMessage = "Model je obavezan")]
+        public string Model { get; set; }
+
+        [BsonElement("Year")]
+        [Required(ErrorMessage = "Godiste je obavezno")]
+        public int Year { get; set; }
+
+        [BsonElement("Price")]
+        [Required(ErrorMessage = "Cena je obavezna")]
+        public decimal Price { get; set; }
+
+        [BsonElement("Images")]
+        public List<BsonBinaryData> Images { get; set; } = new List<BsonBinaryData>();
+
+        [BsonElement("Description")]
+        [Required(ErrorMessage = "Opis vozila je obavezan")]
+        public string Description { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        //Da bi mogla da prikazem koji korisnik je postavio oglas
+        public ApplicationUser User {  get; set; }
+
+    }
+}
+
