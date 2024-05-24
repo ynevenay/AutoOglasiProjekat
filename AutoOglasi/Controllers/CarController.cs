d23014bad54da7d32079c4b38d134c6efc9b58cd
Luka Lazarevic <lukalazarevic001@gmail.com>
2024-05-24 15:02:42 +0200

    CarController - Pregledaj sve oglase i filtriranje

--- AutoOglasi/Controllers/CarController.cs
+++ AutoOglasi/Controllers/CarController.cs
@@ -25,5 +25,35 @@ namespace AutoOglasi.Controllers
             _car = database.GetCollection<Car>("Cars");//inicijalizacija kolekcije
             _userManager = userManager;
         }
+
+        //pregledaj sve oglase
+        public async Task<IActionResult> Index(string brand,string year)
+        {
+            //filtriraj po godini proizvodnje
+            var filterBuilder = Builders<Car>.Filter;
+            var filter = filterBuilder.Empty;
+
+            if (!string.IsNullOrEmpty(brand))
+            {
+                filter = filterBuilder.And(filter, filterBuilder.Eq(c => c.Brand, brand));
+
+            }
+            if (!string.IsNullOrEmpty(year))
+            {
+                if (int.TryParse(year, out int yearValue))
+                {
+                    filter = filterBuilder.And(filter, filterBuilder.Eq(c => c.Year, yearValue));
+                }
+                else
+                {
+                    Console.WriteLine("Greska");
+                }
+            }
+
+
+
+            var oglasi = await _car.Find(filter).ToListAsync();
+            return View(oglasi);
+        }
     }
 }
