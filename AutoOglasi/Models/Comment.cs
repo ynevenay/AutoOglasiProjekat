7ee64da0f9c8b5126e032a5649915df552e6dca5
Tatjana Djukic <tdjukic2821it@raf.rs>
2024-05-25 22:01:52 +0200

    Napravila Model za komenatare

---
+++ AutoOglasi/Models/Comment.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+
+namespace AutoOglasi.Models
+{
+    public class Comment
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonRequired]
+        public string UserId { get; set; }
+
+        [BsonRequired]
+        public string Text { get; set; }
+
+        [BsonRequired]
+        public DateTime CreatedAt { get; set; }
+        //Da bi mogla da prikazem koji korisnik je postavio komentar
+        [BsonIgnore]
+        public ApplicationUser User { get; set; }
+    }
+}
